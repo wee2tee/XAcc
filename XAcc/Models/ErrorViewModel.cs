6adc5a035291181c4cf8ceea667db0582bcec3d2
Weerawat Tarujettanarom <weerawat.36@gmail.com>
2018-03-30 10:46:51 +0700

    Add project files.

---
+++ XAcc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XAcc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
