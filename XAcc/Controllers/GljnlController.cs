18fd826cd33fbb60043c11d09790a258f3c23007
Weerawat Tarujettanarom <weerawat.36@gmail.com>
2018-06-18 17:09:23 +0700

    Gljnl in process

---
+++ XAcc/Controllers/GljnlController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace XAcc.Controllers
+{
+    public class GljnlController : Controller
+    {
+        [Authorize]
+        public IActionResult Index(string trnstat = "U")
+        {
+            if (trnstat == "U")
+                return View("IndexUnpost");
+
+            if (trnstat == "P")
+                return View("IndexPost");
+
+            return View("IndexUnpost");
+        }
+    }
+}
